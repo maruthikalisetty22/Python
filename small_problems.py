edbc0eb2f7fa8041f68a9d4d262fe9b335a15416
maruthikalisetty22 <kalisettymaruthi@gmail.com>
2025-04-07 20:29:34 +0530

    Update small_problems.py

--- small_problems.py
+++ small_problems.py
@@ -1,5 +1,4 @@
 #Return every character in a string 3 times
-
 def paperdoll(str):
     new_str=""
     for i in str:
@@ -9,3 +8,20 @@ def paperdoll(str):
             
 str = "missisipi"
 paperdoll(str)
+
+#Reverse given string
+def masteryoda(str):
+    word_list = str.split()
+    reverse_list = word_list[::-1]
+    reverse_list = " ".join(reverse_list)
+    print(reverse_list)
+  
+str = "Hello how are you"   
+masteryoda(str)
+
+#Absolute function 
+def abs_func(n):
+    return (abs(100-n) <= 10) or (abs(200-n) <= 10)
+    
+output = abs_func(950)
+print(output)
