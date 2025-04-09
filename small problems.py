895f4c67bbdbfbdb9caad26aa08bc7d7c29f00b2
maruthikalisetty22 <kalisettymaruthi@gmail.com>
2025-04-09 16:19:44 +0530

    Update small problems.py

--- small problems.py
+++ small problems.py
@@ -1,3 +1,4 @@
+#Reverse a given string
 def masteryoda(str):
     word_list = str.split()
     reverse_list = word_list[::-1]
@@ -7,9 +8,29 @@ def masteryoda(str):
 str = "Hello how are you"   
 masteryoda(str)
 
-
+#Give required number as ouput
 def abs_func(n):
     return (abs(100-n) <= 10) or (abs(200-n) <= 10)
     
 output = abs_func(950)
 print(output)
+
+#Common letters between the strings
+def common_letters(str1, str2):
+    s1 = set(str1) 
+    s2 = set(str2)
+    common_letters = s1 & s2
+    print(common_letters)
+    
+str1 = "hello"
+str2= "hiddo"
+common_letters(str1, str2)
+
+
+#String to list conversion
+def str_list_conversion(inp_str):
+    str_inp = inp_str.split()
+    print(str_inp)
+    
+inp_str = "Hello dude how are you"
+str_list_conversion(inp_str)
