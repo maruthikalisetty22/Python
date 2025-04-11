027c9bc55fd32306f5674832c8c5364a9c361f98
maruthikalisetty22 <kalisettymaruthi@gmail.com>
2025-04-11 09:39:06 +0530

    Update small_problems.py

--- small_problems.py
+++ small_problems.py
@@ -104,6 +104,86 @@ def up_low(s):
     print(f"Uppercase letters: {upper_list}")
     print(f"Lowercase letters: {lower_list}")
 
+#### Check if given number is in the range of low and high
+
+def check_range(num, low, high):
+    if num in range(low, high+1):
+        print(f"Yes {num} is in the range b/w {low} & {high}")
+    else:
+        print(f"No {num} is not in the range b/w {low} & {high}")
+
+check_range(5, 4, 4)
+
+
+#### Return bolean
+
+def check_range(num, low, high):
+    if num in range(low, high+1):
+        return True
+    else:
+        return False
+        
+output = check_range(5, 4, 6)
+print(output)
+
+## Calculate no of upper case and low case words in a given string
+
+def up_low(s):
+    upper_count = 0
+    lower_count = 0
+    upper_list = []
+    lower_list = []
+
+    for i in s:
+        if i.isupper():
+            upper_count += 1
+            upper_list.append(i)
+        elif i.islower():
+            lower_count += 1
+            lower_list.append(i)
+
+    print(f"This is upper count: {upper_count}")
+    print(f"This is lower count: {lower_count}")
+    print(f"Uppercase letters: {upper_list}")
+    print(f"Lowercase letters: {lower_list}")
+
+## Example usage
+s = "Hello dude How are you doing, Is everything Going good?"
+up_low(s)
+
+
+## Return unique number in the list
+
+def unq_lst(num_lst):
+    final_lst = list(set(num_lst))
+    print(final_lst)
+    
+num_lst = [1,1,1,1,2,2,2,2,3,3,3,3,4,5,6,6,6]
+unq_lst(num_lst)
+
+# Multiply all number in a list
+
+def multiply_numbers(nos):
+    output = 1
+    for i in nos:
+        output *= i
+    print(output)
+
+
+nos = [1, 2, 3, 4, -5]
+multiply_numbers(nos)  # Output: 120
+
+#Check a gven string is palyndorme or not:
+
+def palyndrome(s):
+    if s.replace(" ", "") == s[::-1].replace(" ", ""):
+        print(True)
+    else:
+        print(False)
+        
+s = "nurses run"
+palyndrome(s)
+
 # Example usage
 s = "Hello dude How are you doing, Is everything Going good?"
 up_low(s)
