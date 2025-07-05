9d58889b30da38b70dc5cc1d09f06d4fe3f651ae
maruthikalisetty22 <kalisettymaruthi@gmail.com>
2025-07-05 17:16:34 +0530

    Update and rename pyspark to pyspark.py

---
+++ pyspark.py
@@ -0,0 +1,9 @@
+
+
+
+## Split the names, country and get sum of the 50s and 100s
+
+playerdf = df.withColumn("playername", split(df["player"], "-").getitem(0) \
+             .withColumn("country", split(df['player'], "-").getitem(1)
+            
+finaldf = playerdf.withColumn("sum", col("50s") + col("100s")).filter(col(sum) > 50)
